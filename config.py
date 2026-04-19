9cb69a74204717b99d82c4e8816be7927ca65c8e
surajsaini92044-coder <surajsaini92044@gmail.com>
2026-04-19 07:49:28 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,9 +1,5 @@
-import os
-from dotenv import load_dotenv
+# config.py
+BOT_TOKEN = "8696191334:AAHLsLYTCUJbAgnU0hDkf1lLJjBc9qqg3Yo"
 
-load_dotenv()
-
-BOT_TOKEN = os.getenv("BOT_TOKEN")
-
-if not BOT_TOKEN:
-    raise ValueError("❌ BOT_TOKEN nahi mila! Railway Variables mein BOT_TOKEN add karo.")
+# Example:
+# BOT_TOKEN = "7123456789:AAFxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
