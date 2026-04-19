dadda0a696510f6585d86a264fa239ec7588448c
surajsaini92044-coder <surajsaini92044@gmail.com>
2026-04-19 07:04:11 +0530

    Update states.py

--- states.py
+++ states.py
@@ -1,1 +1,10 @@
+from aiogram.fsm.state import StatesGroup, State
 
+class QuizCreation(StatesGroup):
+    waiting_title = State()
+    waiting_description = State()
+    waiting_question_text = State()
+    waiting_options = State()
+    waiting_correct_option = State()
+    waiting_time_limit = State()
+    waiting_shuffle = State()
