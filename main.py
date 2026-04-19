ae7f32b92551b172fbd4632d91c0eaf78f35c750
surajsaini92044-coder <surajsaini92044@gmail.com>
2026-04-19 07:32:15 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ import asyncio
 from aiogram import Bot, Dispatcher
 from aiogram.fsm.storage.memory import MemoryStorage
 
-# Config
 from config import BOT_TOKEN
 
 # Handlers
@@ -17,17 +16,16 @@ async def main():
     bot = Bot(token=BOT_TOKEN, parse_mode="HTML")
     dp = Dispatcher(storage=MemoryStorage())
 
-    # Sab routers add kar rahe hain
+    # Routers add kar rahe hain
     dp.include_router(commands_router)
     dp.include_router(create_router)
     dp.include_router(take_router)
     dp.include_router(my_quizzes_router)
     dp.include_router(leaderboard_router)
 
-    print("🚀 QuizBot successfully started... @YourQuizBot")
-    print("Bot is now polling...")
+    print("🚀 QuizBot successfully started...")
+    print("Bot is now polling on Railway...")
 
-    # Bot start
     await dp.start_polling(bot)
 
 
